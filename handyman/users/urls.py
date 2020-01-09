f8c55cb25e13e29f26a201be976e4257446bc965
brian13th <brian13th13@gmail.com>
2020-01-09 15:19:04 +0200

    implement user login and logout with respective html templates

--- handyman/users/urls.py
+++ handyman/users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 name = 'users'
 
 urlpatterns = [
     path('register/', views.register, name='users-register'),
+    path('profile/', views.profile, name='users-profile'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='users-login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='users-logout'),
 ]
--- handyman/users/views.py
+++ handyman/users/views.py
@@ -26,3 +26,18 @@ def register(request):
         form = UserRegistrationForm()
         form_p = UserPropertyForm()
     return render(request, 'users/register.html', {'form':form, 'form_p':form_p})
+
+def profile(request):
+    if request.method == 'POST':
+        form_prof = ProfileForm(request.POST, instance=request.user)
+        form_prop = UserPropertyForm(request.POST, instance=request.user.property)
+
+        if form_prof.is_valid() and form_prop.is_valid():
+            form_prof.save()
+            form_prop.save()
+            messages.success(request, f'You have successfully updated your profile!')
+            return redirect('users-profile')
+    else:
+        form_prof = ProfileForm(instance=request.user)
+        form_prop = UserPropertyForm(instance=request.user.property)
+    return render(request, 'users/profile.html', {'form_prop': form_prop, 'form_prof':form_prof})
