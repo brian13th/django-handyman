4a2bdf8c679855f5e46f90cfec858f3cf1db48d5
brian13th <brian13th13@gmail.com>
2020-01-09 03:41:05 +0200

    adding model Post and crispy forms app

--- handyman/handyman/settings.py
+++ handyman/handyman/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
